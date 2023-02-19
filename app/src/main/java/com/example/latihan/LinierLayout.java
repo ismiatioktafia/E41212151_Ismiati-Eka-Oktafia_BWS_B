ab2048fbbda5908260585c74eca2ea8d88f6cc5e
ismiatioktafia <ismiatiekaokavia@gmail.com>
2023-02-19 19:00:50 +0700

    Minggu1

---
+++ app/src/main/java/com/example/latihan/LinierLayout.java
@@ -0,0 +1,21 @@
+package com.example.latihan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+public class LinierLayout extends AppCompatActivity {
+    EditText to, subject, message;
+    Button send;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linier_layout);
+
+        to = findViewById(R.id.to);
+        subject = findViewById(R.id.subject);
+        message = findViewById(R.id.message);
+        send = findViewById(R.id.send);
+    }
+}
